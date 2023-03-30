8f0476facc05d96c6f22d1f9743665d7d23d402a
Koushik Mandal <106190199+koushikmandalofficial@users.noreply.github.com>
2023-03-30 08:14:33 +0530

    Add files via upload

---
+++ JAVA/Assignment2/Answer3/mypackage/Circle.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle.");
+    }
+}
---
+++ JAVA/Assignment2/Answer3/mypackage/Shape.java
@@ -0,0 +1,7 @@
+package mypackage;
+
+public class Shape {
+    public void draw() {
+        // no definition
+    }
+}
