349e970421cfdaeb3291fc0eaee75db479b9c589
Koushik Mandal <106190199+koushikmandalofficial@users.noreply.github.com>
2023-04-06 08:27:19 +0530

    Add files via upload

---
+++ JAVA/Assignment3/Answer4/register/Register.java
@@ -0,0 +1,26 @@
+package register;
+import mypackage.College;
+import mypackage.Student;
+public class Register {
+
+    
+    public static void main(String[] args) {
+        
+        
+        College college1 = new College(130, "Techno Main Salt Lake");
+        College college2 = new College(779, "Techno India University");
+
+        Student student1 = new Student(1, "Koushik Mandal", "MCA", 2022,90);
+        Student student2 = new Student(1, "Snahangshu Sarkar", "MCA", 2022,92);
+        Student student3 = new Student(3, "Sutanu Mahata", "MCA", 2022, 92);
+        Student student4 = new Student(3, "Rishav Mandal", "B.Tech (CS)", 2022, 92);
+        
+        college1.addStudent(student1);  
+        college1.addStudent(student2);
+        college1.addStudent(student3); 
+        college2.addStudent(student4); 
+        college2.addStudent(student3); 
+       
+    }
+    
+}
