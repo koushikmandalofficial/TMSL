631fc8e34edac69273a903d30a2ee001e93e8e60
Koushik Mandal <106190199+koushikmandalofficial@users.noreply.github.com>
2023-05-14 21:18:00 +0530

    Update StudentExample.java

--- JAVA/Assignment4/Answer10/studentexample/StudentExample.java
+++ JAVA/Assignment4/Answer10/studentexample/StudentExample.java
@@ -5,12 +5,11 @@ import mypackage.StudentManager;
 public class StudentExample {
     public static void main(String[] args) {
         StudentManager studentManager = new StudentManager();
-        studentManager.addStudent(new Student("Alice", 1, 95.0, "2022"));
-        studentManager.addStudent(new Student("Bob", 2, 80.0, "2022"));
-        studentManager.addStudent(new Student("Charlie", 3, 70.0, "2022"));
-        studentManager.addStudent(new Student("David", 4, 85.0, "2022"));
-        studentManager.addStudent(new Student("Eve", 5, 90.0, "2022"));
-
+        studentManager.addStudent(new Student("Ram", 1, 95.0, "2022"));
+        studentManager.addStudent(new Student("Shyam", 2, 80.0, "2022"));
+        studentManager.addStudent(new Student("Jodu", 3, 70.0, "2022"));
+        studentManager.addStudent(new Student("Modhu", 4, 85.0, "2022"));
+        studentManager.addStudent(new Student("Gopal", 5, 90.0, "2022"));
         studentManager.writeToFile("file8.txt");
         studentManager.readFromFile("file8.txt");
     }
